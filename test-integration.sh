bf8152864e67e60d775f14a1f34825a3dd4d31b5
Mark Polyakov <mark@markasoftware.com>
2018-08-10 23:29:27 -0700

    more tests, more ways to detect compatibility

---
+++ test-integration.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+# Execute this file to run integration tests on Anypaste.
+# These tests use a custom, offline, set of plugins so that this test is reproducible.
+# Therefore, the built-in plugins are not covered by these tests.
+
+# shellcheck disable=2016
+# shellcheck disable=2034
+
+shopt -s extglob
+
+function exit {
+	last_exit_code="$?"
+}
+
+function anypaste {
+	source ./anypaste "$@" 2>&1
+}
+
+function oneTimeSetUp() {
+	if [[ ! -t 0 ]]
+	then
+		echo 'ERROR: The test must be run with stdin coming from a terminal, not a pipe or file.'
+		return 1
+	fi
+}
+
+function test_no_args() {
+	local out exit_code
+	out=$(ap_main 2>&1)
+	exit_code="$?"
+	assertEquals 'exit code' 102 "$exit_code"
+	assertTrue 'prints error message' '[[ $out == *ERROR* ]]'
+	assertTrue 'prints help text' '[[ $out == *OPTIONS* ]]'
+}
+
+function test_help() {
+	local out_h out_help out_stdout exit_code
+	out_h=$(ap_main -h 2>&1)
+	out_help=$(ap_main --help 2>&1)
+	exit_code="$?"
+	assertTrue 'prints help text' '[[ $out_h == *OPTIONS* ]]'
+	assertEquals '-h and --help are identical' "$out_h" "$out_help"
+	assertEquals 'exit code' 0 "$exit_code"
+}
+
+function test_version() {
+	local out exit_code
+	out=$(ap_main -v 2>&1)
+	exit_code="$?"
+	assertEquals 'exit code' 0 "$exit_code"
+	assertTrue 'prints version' '[[ $out == Anypaste" "?.??(.?) ]]'
+}
+
+# @param $1 the stdout/err from anypaste
+function is_url_anypaste() {
+	local curl_output
+	curl_output=$(curl -s $(grep -oP '(?<=Direct: ).*' <<< "$1"))
+	assertEquals 'url equals anypaste' "$curl_output" "$(<anypaste)"
+}
+
+function test_basic_upload() {
+	local out exit_code
+	out=$(ap_main ./anypaste 2>&1)
+	exit_code="$?"
+	assertEquals 'exit code' 0 "$exit_code"
+	assertTrue 'uploads to Hastebin' '[[ $out == *https://hastebin.com/+([a-z])* ]]'
+	is_url_anypaste "$out"
+}
+
+function test_p_upload() {
+	local out exit_code
+	out=$(ap_main -p ixio ./anypaste 2>&1)
+	exit_code="$?"
+	assertEquals 'exit code' 0 "$exit_code"
+	assertTrue 'uploads to ixio' '[[ $out == *http://ix.io/+([a-zA-Z0-9])* ]]'
+	is_url_anypaste "$out"
+}
+
+function test_stdin_upload() {
+	local out exit_code
+	out=$(ap_main < ./anypaste 2>&1)
+	exit_code="$?"
+	assertEquals 'exit code' 0 "$exit_code"
+	assertTrue 'uploads to Hastebin' '[[ $out == *https://hastebin.com/+([a-z])* ]]'
+	is_url_anypaste "$out"
+}
+
+function test_multi_upload() {
+	local out exit_code
+	out=$(ap_main -c fixtures/configs/essentials.conf ./anypaste ./LICENSE)
+	echo "$out"
+	exit_code="$?"
+	assertEquals 'exit code' 0 "$exit_code"
+	assertTrue 'uploads anypaste' '[[ $out == *"essentials upload anypaste" ]]'
+	assertTrue 'uploads LICENSE' '[[ $out == *"essentials upload LICENSE" ]]'
+}
+
+ap_test=true
+source ./anypaste
+for i in ./fixtures/plugins/*; do source "$i"; done
+source ./shunit2/shunit2
--- test.sh
+++ test.sh
@@ -97,6 +97,99 @@ test_ap_filter_global_plugins() {
 
 test_ap_filter_local_plugins() {
 	local ap_local_plugins
+	ap_local_plugins=('essentials')
+	ap_filter_local_plugins >/dev/null
+	assertEquals 'single compatible plugin' 'essentials' "${ap_local_plugins[*]}"
+
+	ap_local_plugins=('essentials' 'taggy_1')
+	ap_filter_local_plugins >/dev/null
+	assertEquals 'two compatible plugins' 'essentials taggy_1' "${ap_local_plugins[*]}"
+
+	ap_local_plugins=('incompatible')
+	ap_filter_local_plugins >/dev/null
+	assertNull 'one incompatible plugin' "${ap_local_plugins[*]}"
+
+	ap_local_plugins=('essentials' 'incompatible' 'taggy_1')
+	ap_filter_local_plugins >/dev/null
+	assertEquals 'incompatible surrounded by two compatible plugins' 'essentials taggy_1' "${ap_local_plugins[*]}"
+}
+
+test_ap_summary() {
+	local ap_ok_uls ap_fail_uls out
+	ap_ok_uls=('a' 'b')
+	ap_fail_uls=()
+	out=$(ap_summary 2>&1)
+	assertEquals 'two sucessful uploads' "Sucessfully uploaded: 'a' 'b'" "$out"
+
+	ap_ok_uls=('a')
+	ap_fail_uls=('b')
+	out=$(ap_summary 2>&1)
+	assertEquals 'one ok, one fail' "Sucessfully uploaded: 'a'
+Failed to upload: 'b'" "$out"
+
+	ap_ok_uls=()
+	ap_fail_uls=('a' 'b')
+	out=$(ap_summary 2>&1)
+	assertEquals 'two fails only' "Sucessfully uploaded: None
+Failed to upload: 'a' 'b'" "$out"
+}
+
+test_json_parse() {
+	local out
+	out=$(json_parse '{"hello":"world"}' 'hello')
+	assertEquals 'basic json' 'world' "$out"
+
+	out=$(json_parse '{"hello":42}' 'hello')
+	assertEquals 'numerical (no quotes)' '42' "$out"
+
+	out=$(json_parse '"{\"doop\":\"why do you like me?\"}"' 'doop')
+	assertEquals 'escaped quotes fuck' 'why do you like me?' "$out"
+
+	out=$(json_parse '{"hello":"world"}' 'hello')
+	assertEquals 'basic json' 'world' "$out"
+
+	out=$(json_parse '{  "hello"  : "world"   }' 'hello')
+	assertEquals 'extra cosmetic spaces' 'world' "$out"
+
+	out=$(json_parse '{  "h e l  l o"  : "wor  ld"   }' 'h e l  l o')
+	assertEquals 'extra spaces in keys/values' 'wor  ld' "$out"
+
+	out=$(json_parse '{"hello":"world","foo":"bar","cat":"eat dog"}' 'foo')
+	assertEquals 'simple multiprop' 'bar' "$out"
+
+	out=$(json_parse '{  "hello" : "world"  , "f oo" :"ba r"  , "cat": "eat dog"  }' 'f oo')
+	assertEquals 'multiprop with some extra spaces' 'ba r' "$out"
+
+	out=$(json_parse '{"hello":"world","world":"hello"}' 'world')
+	assertEquals 'key name appears earlier' 'hello' "$out"
+}
+
+test_upload_loop() {
+	ap_i=false
+	ap_f=false
+	ap_list=false
+
+	# no plugins at all
+	ap_user_path=a
+	ap_local_plugins=()
+	out=$(upload_loop 2>&1)
+	exit_code=$?
+	assertEquals 'failed' '1' "$exit_code"
+	assertTrue 'correct error message' '[[ $out == *"No compatible plugins found"* ]]'
+
+	# one sucessful plugin
+	ap_user_path=a
+	ap_local_plugins=('essentials')
+	out=$(upload_loop 2>&1)
+	exit_code=$?
+	assertEquals 'suceeded' '0' "$exit_code"
+	assertTrue 'uploaded the essentials' '[[ $out == *"essentials upload"* ]]'
+
+	ap_user_path=a
+	ap_local_plugins=('upload_fail')
+	out=$(upload_loop 2>&1)
+	exit_code=$?
+	assertEquals 'failed' '1' "$exit_code"
 }
 
 ap_test='true'
